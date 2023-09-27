c7a9ba514d02a6891cf4bc1b212910fb67efc0bf
Bad <BadWolf42@users.noreply.github.com>
2023-09-27 11:00:20 +0200

    CONFIG: Remove dead html

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -21,26 +21,3 @@ if (!isConnect()) {
   include_file('desktop', '404', 'php');
   die();
 }
-/*
-?>
-<form class="form-horizontal">
-  <fieldset>
-    <div class="form-group">
-      <label class="col-md-4 control-label">{{Port du démon}}
-        <sup><i class="fas fa-question-circle tooltips" title="{{Renseignez un autre port uniquement si celui-ci est déjà utilisé sur votre Jeedom}}"></i></sup>
-      </label>
-      <div class="col-md-4">
-        <input class="configKey form-control" data-l1key="socketport" placeholder="56023">
-      </div>
-    </div>
-    <div class="form-group">
-      <label class="col-md-4 control-label">{{Cycle (en secondes)}}
-        <sup><i class="fas fa-question-circle tooltips" title="{{Cycle de mise à jour des données vers Jeedom}}"></i></sup>
-      </label>
-      <div class="col-md-4">
-        <input class="configKey form-control" data-l1key="cycle" placeholder="5">
-      </div>
-    </div>
-  </fieldset>
-</form>
-*/
